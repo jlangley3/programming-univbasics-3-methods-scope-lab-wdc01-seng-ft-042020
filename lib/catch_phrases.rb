0c961b47a4d10a89cbb1310d253787cde206063e
Jesse Langley <jlangleyiii@aol.com>
2020-04-08 17:05:45 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,11 +1,19 @@
-def link
-  toadstool
+def mario
   phrase = "It's-a me, Mario!"
   puts phrase
+  toadstool
 end
 
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  toadstool
+  phrase = "It's-a me, Mario!"
+  puts phrase
+end
+
+
 
